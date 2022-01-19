79d48804bfa4191de2eac7abddde81196b0e964b
akshankdahiya <97908366+akshankdahiya@users.noreply.github.com>
2022-01-19 21:46:40 +0530

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,28 +8,44 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top == MAX - 1){
+            system.out.println("Stack Overflow");
+            return false;
+        }
+        top++;
+        a[top] = x;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top == -1){
+            system.out.println("Stack Underflow");
+            return a[top--];
+        }
+        
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top == -1){
+            return a[top];
+        }
     } 
 } 
   
